8883077ff47136949e3925646b8b96fb77515d55
kuro12237 <115063113+kuro12237@users.noreply.github.com>
2023-04-19 16:53:19 +0900

    01_02_確認課題

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -1,19 +1,74 @@
 #include "GameScene.h"
 #include "TextureManager.h"
 #include <cassert>
+#include<ImGuiManager.h>
+#include<PrimitiveDrawer.h>
+#include "AxisIndicator.h"
+GameScene::GameScene() 
+{
 
-GameScene::GameScene() {}
+}
 
-GameScene::~GameScene() {}
-
-void GameScene::Initialize() {
-
+GameScene::~GameScene()
+{
+	delete sprite_;
+	delete model_;
+	delete debugCamera_;
+}
+void GameScene::Initialize() 
+{
+    //ライン描画
+	PrimitiveDrawer::GetInstance()->SetViewProjection(&viewProjection_);
+	
 	dxCommon_ = DirectXCommon::GetInstance();
 	input_ = Input::GetInstance();
 	audio_ = Audio::GetInstance();
+	// 音
+	soundDataHandle_ = audio_->LoadWave("fanfare.wav");
+	// 2D
+	textureHandle_ = TextureManager::Load("kamata.ico");
+	// 3D
+	textureModelHandle_ = TextureManager::Load("kamata.ico");
+
+	// 2D
+	sprite_ = Sprite::Create(textureHandle_, {100, 50});
+	// 3D
+	model_ = Model::Create();
+	
+
+	// ワールドトランスフォーム
+	worldTransform_.Initialize();
+	// ビュープロジェクション
+	viewProjection_.Initialize();
+	//でバックカメラの生成
+	debugCamera_ = new DebugCamera(1280, 720);
+    
+	AxisIndicator::GetInstance()->SetVisible(true);
+	AxisIndicator::GetInstance()->SetTargetViewProjection(&debugCamera_->GetViewProjection());
 }
 
-void GameScene::Update() {}
+void GameScene::Update() 
+{ Vector2 position = sprite_->GetPosition();
+	position.x += 2.0f;
+	position.y += 1.0f;
+	sprite_->SetPosition(position);
+
+	if (input_->TriggerKey(DIK_W))
+	{
+		// 音声再生
+		audio_->PlayWave(soundDataHandle_);
+
+	}
+	debugCamera_->Update();
+
+	
+	ImGui::Text("aiueo %f", 4090.0f);
+	ImGui::Begin("Debug1");
+	ImGui::InputFloat3("InputFloat3", inputFloat3);
+	ImGui::SliderFloat3("SliderFloat3", inputFloat3, 0.0f, 1.0f);
+	ImGui::End();
+	ImGui::ShowDemoWindow();
+}
 
 void GameScene::Draw() {
 
@@ -24,12 +79,19 @@ void GameScene::Draw() {
 	// 背景スプライト描画前処理
 	Sprite::PreDraw(commandList);
 
+
 	/// <summary>
 	/// ここに背景スプライトの描画処理を追加できる
 	/// </summary>
+	sprite_->Draw();
+
 
 	// スプライト描画後処理
 	Sprite::PostDraw();
+	// ライン描画
+	PrimitiveDrawer::GetInstance()->DrawLine3d({0, 0, 0}, {0, 10, 0}, {1.0f, 0.0f, 0.0f, 1.0f});
+
+
 	// 深度バッファクリア
 	dxCommon_->ClearDepthBuffer();
 #pragma endregion
@@ -37,10 +99,11 @@ void GameScene::Draw() {
 #pragma region 3Dオブジェクト描画
 	// 3Dオブジェクト描画前処理
 	Model::PreDraw(commandList);
-
+	model_->Draw(worldTransform_, debugCamera_->GetViewProjection(), textureModelHandle_);
 	/// <summary>
 	/// ここに3Dオブジェクトの描画処理を追加できる
 	/// </summary>
+	model_->Draw(worldTransform_, viewProjection_, textureModelHandle_);
 
 	// 3Dオブジェクト描画後処理
 	Model::PostDraw();
--- scene/GameScene.h
+++ scene/GameScene.h
@@ -8,6 +8,7 @@
 #include "Sprite.h"
 #include "ViewProjection.h"
 #include "WorldTransform.h"
+#include"DebugCamera.h"
 
 /// <summary>
 /// ゲームシーン
@@ -18,7 +19,10 @@ public: // メンバ関数
 	/// <summary>
 	/// コンストクラタ
 	/// </summary>
-	GameScene();
+	GameScene(
+
+	
+	);
 
 	/// <summary>
 	/// デストラクタ
@@ -43,7 +47,24 @@ public: // メンバ関数
 private: // メンバ変数
 	DirectXCommon* dxCommon_ = nullptr;
 	Input* input_ = nullptr;
+	
 	Audio* audio_ = nullptr;
+	Sprite* sprite_ = nullptr;
+	Model* model_ = nullptr;
+	//ワールドトランスフォーム
+	WorldTransform worldTransform_;
+	//ビュープロジェクション
+	ViewProjection viewProjection_;
+
+
+	 uint32_t textureHandle_ = 0;
+	 uint32_t textureModelHandle_ = 0;
+	 
+	 uint32_t soundDataHandle_ = 0;
+	 uint32_t voiceHandle_ = 0;
+	 float inputFloat3[3] = {0, 0, 0};
+
+	 DebugCamera* debugCamera_ = nullptr;
 
 	/// <summary>
 	/// ゲームシーン用
