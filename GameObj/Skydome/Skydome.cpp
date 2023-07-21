fbb5f06574f21bc195f901b077cb9626c82d7dbc
kuro12237 <115063113+kuro12237@users.noreply.github.com>
2023-07-21 16:04:13 +0900

    02_11_確認課題

--- GameObj/Skydome/Skydome.cpp
+++ GameObj/Skydome/Skydome.cpp
@@ -15,7 +15,7 @@ void Skydome::Initialize()
 	modelSkydome_ = Model::CreateFromOBJ("CelestialSphere", true);
 	worldTransform_.Initialize();
 	
-	//worldTransform_.scale_ = {100, 100, 100};
+	
 	worldTransform_.scale_ = {100.0f, 100.0f, 100.0f};
 
 }
@@ -23,14 +23,6 @@ void Skydome::Initialize()
 void Skydome::Update() 
 {
 	
-	//worldTransform_.TransferMatrix();
-
-	//worldTransform_.translation_ = {0.0f, 0.0f, 0.0f};
-
-	
-
-	//worldTransform_.matWorld_ = MakeAffineMatrix(
-	 //   worldTransform_.scale_, worldTransform_.rotation_, worldTransform_.translation_);
 
 	// 更新処理
 	worldTransform_.UpdateMatrix();
