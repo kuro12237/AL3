b514afd811b0ea24f43458c277f8e8c4b37d227a
kuro12237 <115063113+kuro12237@users.noreply.github.com>
2023-08-03 13:54:47 +0900

    02_15_確認課題

--- math/Matrix/MatrixTrans.cpp
+++ math/Matrix/MatrixTrans.cpp
@@ -447,16 +447,28 @@ Matrix4x4 MakeViewportMatrix(
 	return result;
 }
 
-Vector3 Transform(const Vector3& vector, const Matrix4x4& matrix) {
+Vector3 Transform(const Vector3& v, const Matrix4x4& matrix) {
 	Vector3 result;
-	result.x = vector.x * matrix.m[0][0] + vector.y * matrix.m[1][0] + vector.z * matrix.m[2][0] +
-	           1.0f * matrix.m[3][0];
-	result.y = vector.x * matrix.m[0][1] + vector.y * matrix.m[1][1] + vector.z * matrix.m[2][1] +
-	           1.0f * matrix.m[3][1];
-	result.z = vector.x * matrix.m[0][2] + vector.y * matrix.m[1][2] + vector.z * matrix.m[2][2] +
-	           1.0f * matrix.m[3][2];
-	float W = vector.x * matrix.m[0][3] + vector.y * matrix.m[1][3] + vector.z * matrix.m[2][3] +
-	          1.0f * matrix.m[3][3];
+	result.x = 
+		  v.x * matrix.m[0][0] 
+		+ v.y * matrix.m[1][0]
+		+ v.z * matrix.m[2][0]
+		+1.0f * matrix.m[3][0];
+	result.y = 
+		  v.x * matrix.m[0][1] 
+		+ v.y * matrix.m[1][1] 
+		+ v.z * matrix.m[2][1] 
+		+1.0f * matrix.m[3][1];
+	result.z = 
+		  v.x * matrix.m[0][2] 
+		+ v.y * matrix.m[1][2]
+		+ v.z * matrix.m[2][2]
+		+1.0f * matrix.m[3][2];
+	float W =
+		  v.x * matrix.m[0][3] 
+		+ v.y * matrix.m[1][3]
+		+ v.z * matrix.m[2][3]
+		+1.0f * matrix.m[3][3];
 	assert(W != 0.0f);
 	result.x /= W;
 	result.y /= W;
